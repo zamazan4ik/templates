fad96f50857288a3f9c22fde52d45d091d22b38b
dawe <dawedawe@posteo.de>
2025-03-16 18:51:33 +0100

    Add authors override to just file (#99)

--- event-driven-generated/src/app.rs
+++ event-driven-generated/src/app.rs
@@ -1,7 +1,7 @@
 use crate::event::{AppEvent, Event, EventHandler};
 use ratatui::{
-    crossterm::event::{KeyCode, KeyEvent, KeyModifiers},
     DefaultTerminal,
+    crossterm::event::{KeyCode, KeyEvent, KeyModifiers},
 };
 
 /// Application.
